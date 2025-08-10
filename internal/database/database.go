262068fc46fda37b54e7c45f6c9004ac5e665c0b
Daniel Salazar <dsnile.salazr@gmail.com>
2025-08-09 18:09:46 -0600

    retry connection to db

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/dsniels/market/core/types"
 	"gorm.io/driver/sqlserver"
@@ -20,18 +23,44 @@ func Connect() *gorm.DB {
 	url := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
 		server, user, password, port, database,
 	)
-	db, err := gorm.Open(sqlserver.Open(url), &gorm.Config{})
-	if err != nil {
-		slog.Error("Open Database", err.Error())
-		panic(err)
-	}
-
+	slog.Info("Connecting to database")
+	db := connectDb(url)
 	slog.Info("Database connected")
 	return db
 }
 
 func Migrate(db *gorm.DB) error {
-	slog.Info("Starting Migrations")
+	slog.Info("Migrating database")
 	return db.AutoMigrate(&types.Sucursal{}, &types.Categoria{}, &types.Producto{}, &types.FormaPago{}, &types.ProductoCompuesto{})
+}
 
+func connectDb(url string) *gorm.DB {
+
+	d, err := sql.Open("sqlserver", url)
+	if err != nil {
+		log.Fatalln("Cannot connect db", err)
+	}
+
+	tries := 10
+
+	for {
+		err := d.Ping()
+		if err != nil {
+			if tries == 0 {
+				log.Fatalln("something went wrong", err)
+			}
+			slog.Warn("Couldnt connect to db", slog.Int("retries left", tries))
+			tries--
+			time.Sleep(3 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	db, err := gorm.Open(sqlserver.Open(url))
+	if err != nil {
+		log.Fatalln("Cannot connect db", err)
+	}
+
+	return db
 }
